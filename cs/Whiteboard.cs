0c0eb158a4d0108786c1f0dccb642341c6a72b23
hyrious <hyrious@outlook.com>
2022-04-19 15:15:53 +0800

    avoid `async void`

--- cs/Whiteboard.cs
+++ cs/Whiteboard.cs
@@ -191,7 +191,7 @@ namespace Whiteboard {
         /// <summary>
         /// Load streaming-assets://index.html with room info.
         /// </summary>
-        public async void JoinRoom(string appIdentifier, string region, string uid, string uuid, string roomToken) {
+        public async Task JoinRoom(string appIdentifier, string region, string uid, string uuid, string roomToken) {
             await webViewPrefab.WaitUntilInitialized();
 
             readySource = new TaskCompletionSource<bool>();
